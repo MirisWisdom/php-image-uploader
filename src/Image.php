a1288b7987a61eac2c211c588248d1b48b0a31d6
Roman Emilian <roman.emilian@outlook.com>
2017-07-04 08:38:17 +0800

    feat(model) add constructor to image class

--- src/Image.php
+++ src/Image.php
@@ -9,5 +9,14 @@
  */
 class Image
 {
-    //
+    private $realName;
+    private $tempName;
+    private $fileSize;
+
+    public function __construct(array $image)
+    {
+        $this->realName = $image['name'];
+        $this->tempName = $image['tmp_name'];
+        $this->fileSize = $image['size'];
+    }
 }
