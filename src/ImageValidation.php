5988357e51addc9f410d323bf38633995edda569
Roman Emilian <roman.emilian@outlook.com>
2017-07-04 08:36:52 +0800

    init(model) image validation class

---
+++ src/ImageValidation.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Class ImageValidation
+ *
+ * A class for validating images based on GitHugop's business rules.
+ *
+ * @author Roman Emilian <roman.emilian@outlook.com>
+ */
+class ImageValidation
+{
+    //
+}
