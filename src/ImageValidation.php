bcaca611a0b4e974e220c4cc8b0730ff77c6d28c
Roman Emilian <roman.emilian@outlook.com>
2017-07-04 09:07:11 +0800

    docs(model) documentation for the image validation functions

--- src/ImageValidation.php
+++ src/ImageValidation.php
@@ -15,26 +15,53 @@ class ImageValidation
 
     private $image;
 
+    /**
+     * ImageValidation constructor.
+     *
+     * @param Image $image  Instance of the Image object
+     */
     public function __construct(Image $image)
     {
         $this->image = $image;
     }
 
+    /**
+     * Determines if the file extension for the file is valid.
+     *
+     * @return bool The extension is valid.
+     */
     public function isExtensionValid() : bool
     {
+        // Accepted extensions: JP(E)G and PNG
         return preg_match("/\.(jpe?g|png)\b/", $this->image->getRealName());
     }
 
+    /**
+     * Determines whether the file is bigger than the maximum size or not.
+     *
+     * @param   int $maxSize    The maximum size for the file.
+     * @return  bool            File is not bigger than the maximum size.
+     */
     public function isSizeValid(int $maxSize) : bool
     {
         return !$this->image->getFileSize() > $maxSize;
     }
 
+    /**
+     * Verifies that the file is an actual image file.
+     *
+     * @return bool The input file is an actual image.
+     */
     public function isValidImage() : bool
     {
         return is_array(getimagesize($this->image->getTempName()));
     }
 
+    /**
+     * Verifies if the MIME type for the image is valid based on EXIF metadata.
+     *
+     * @return bool The MIME type is valid.
+     */
     public function isValidType() : bool
     {
         $validMimes = array('image/jpeg', 'image/png');
